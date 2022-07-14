57eae5e281cfd72175b9c279374490e369e9cd53
PREVIRED\smasferrer <smasferrer@previred.com>
2022-07-14 10:12:18 -0400

    ultimos cambios

--- src/views/user-generator/UserGenerator.vue
+++ src/views/user-generator/UserGenerator.vue
@@ -1,21 +1,23 @@
 <script setup>
+import { Search } from '@element-plus/icons-vue'
 </script>
 
 <template>
   <div class="header-content">
-    </div>
+    <h1>Título del contenido</h1>
+  </div>
 
   <!-- Tabla -->
   <el-row :gutter="20">
     <el-col :span="24">
       <div class="cards">
         <b-card no-body>
-            <h3>Tabla</h3>
-            <el-table :data="tableData" class="tabla">
-              <el-table-column prop="date" label="Date" width="180" />
-              <el-table-column prop="name" label="Name" width="180" />
-              <el-table-column prop="address" label="Address" />
-            </el-table>
+          <h3>Tabla</h3>
+          <el-table :data="tableData" class="tabla">
+            <el-table-column prop="date" label="Date" width="180" />
+            <el-table-column prop="name" label="Name" width="180" />
+            <el-table-column prop="address" label="Address" />
+          </el-table>
         </b-card>
       </div>
     </el-col>
@@ -23,30 +25,90 @@
   <!-- fin tabla -->
   <!-- Buttons -->
   <el-row :gutter="20">
-    <el-col :span="12">
+    <el-col :span="24">
       <div class="grid-content ep-bg-purple" />
       <div class="cards">
         <div class="components">
           <b-card no-body>
             <h3>Botones</h3>
-            <el-row class="mb-4">
-              <button class="pt-primary">Primary</button>
-              <button class="pt-secondary">Secondary</button>
-              <button class="pt-other">other</button>
+            <el-row :gutter="20">
+              <el-col :span="12">
+                <h5>Activos</h5>
+                <el-row class="mb-4">
+                  <button class="pt-primary">Primary</button>
+                  <button class="pt-secondary">Secondary</button>
+                  <button class="pt-other">other</button>
+                </el-row>
+              </el-col>
+              <el-col :span="12">
+                <h5>Deshabilitados</h5>
+                <el-row class="mb-4">
+                  <button class="pt-primary" disabled>Primary</button>
+                  <button class="pt-secondary" disabled>Secondary</button>
+                  <button class="pt-other" disabled>other</button>
+                </el-row>
+              </el-col>
             </el-row>
           </b-card>
         </div>
       </div>
     </el-col>
-    <el-col :span="12">
-      <div class="grid-content ep-bg-purple" />Es el mismo
+  </el-row>
+  <!-- Fin buttons -->
+  <!-- Inputs -->
+  <el-row :gutter="20">
+    <!-- Inputs -->
+    <el-col :span="16">
+      <div class="grid-content ep-bg-purple" />
+      <div class="cards">
+        <div class="components">
+          <b-card no-body>
+            <h3>Inputs</h3>
+            <h5>Input Text</h5>
+            <el-row class="mb-2">
+              <el-input v-model="input1" class="w-100 m-2" size="large" placeholder="Escriba algo" />
+              <el-input v-model="input1" class="w-100 m-2" size="large" placeholder="Please Input"
+                :prefix-icon="Search" />
+            </el-row>
+            <h5>Select</h5>
+            <el-row class="mb-2">
+              <el-select v-model="value" class="w-100 m-2" placeholder="Select" size="large">
+                <el-option v-for="item in options" :key="item.value" :label="item.label" :value="item.value" />
+              </el-select>
+            </el-row>
+            <h5>Radios</h5>
+            <el-row class="mb-2">
+              <el-radio-group v-model="radio">
+                <el-radio :label="3">Option A</el-radio>
+                <el-radio :label="6">Option B</el-radio>
+                <el-radio :label="9">Option C</el-radio>
+              </el-radio-group>
+            </el-row>
+          </b-card>
+        </div>
+      </div>
+    </el-col>
+    <!-- Arbolito -->
+    <el-col :span="8">
+      <div class="grid-content ep-bg-purple" />
+      <div class="cards">
+        <div class="components">
+          <b-card no-body>
+            <h3>Tree</h3>
+            <h5>Arbol básico</h5>
+            <el-tree :data="data" show-checkbox node-key="id" :default-expanded-keys="[2, 3]"
+              :default-checked-keys="[5]" :props="defaultProps" />
+          </b-card>
+        </div>
+      </div>
     </el-col>
   </el-row>
-  <!-- card -->
+  <!-- Fin Inputs -->
 
 </template>
 
 <script>
+//Data para table
 export default {
   data() {
     return {
@@ -75,6 +137,93 @@ export default {
     }
   }
 }
+
+//Data para select
+
+const options = [
+  {
+    value: 'Option1',
+    label: 'Option1',
+  },
+  {
+    value: 'Option2',
+    label: 'Option2',
+  },
+  {
+    value: 'Option3',
+    label: 'Option3',
+  },
+  {
+    value: 'Option4',
+    label: 'Option4',
+  },
+  {
+    value: 'Option5',
+    label: 'Option5',
+  },
+]
+
+//Info para radio buttons
+
+import { ref } from 'vue'
+
+const radio = ref(3)
+
+//data para tree (Arbol)
+const defaultProps = {
+  children: 'children',
+  label: 'label',
+}
+const data = [
+  {
+    id: 1,
+    label: 'Nivel 1',
+    children: [
+      {
+        id: 4,
+        label: 'Nivel 1 de 1',
+        children: [
+          {
+            id: 9,
+            label: 'Nivel 1 de 1 de 1 xd',
+          },
+          {
+            id: 10,
+            label: 'Nivel 1 de 1 de 1 xd gemelo',
+          },
+        ],
+      },
+    ],
+  },
+  {
+    id: 2,
+    label: 'Nivel 2',
+    children: [
+      {
+        id: 5,
+        label: 'Hijo del nivel 2',
+      },
+      {
+        id: 6,
+        label: 'Hermano del hijo del nivel 2',
+      },
+    ],
+  },
+  {
+    id: 3,
+    label: 'Nivel 3 tío',
+    children: [
+      {
+        id: 7,
+        label: 'Sobrino favorito del tío',
+      },
+      {
+        id: 8,
+        label: 'El vecino del tío que lo llama tío',
+      },
+    ],
+  },
+]
 </script>
 
 <style lang="scss">
